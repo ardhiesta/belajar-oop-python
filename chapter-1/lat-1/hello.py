e5125dd5ae3dacc9567baf1aeb5a97df58f85b8a
ardhiesta <ardhi.mail@gmail.com>
2024-02-24 11:48:01 +0800

    menambahkan komentar ke hello.py

--- chapter-1/lat-1/hello.py
+++ chapter-1/lat-1/hello.py
@@ -1,1 +1,8 @@
+"""
+hello.py
+ini adalah contoh source code Python sederhana untuk menampilkan teks ke layar
+
+ardhi wijayanto
+"""
 print('hai ini adalah source code bahasa pemrograman Python')
+print('code ini belum menerapkan paradigma pemrograman berorientasi objek')
